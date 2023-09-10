bfd0d76a8bdb1de5334309f04e4ea05d05c7b995
Leon Si <leon@leondreamed.com>
2023-09-10 12:12:49 -0400

    feat: commit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
 import { createTildeImportExpander } from "tilde-imports";
-import { getMonorepoDirpath } from "get-monorepo-root";
 
-/** @type {import('bun').BunPlugin} */
-const tildeImportsPlugin = {
-  name: "bun-plugin-tilde-imports",
-  setup(build) {
-    const monorepoDirpath = getMonorepoDirpath(__dirname);
-    if (monorepoDirpath === undefined) {
-      throw new Error("Could not get monorepo dirpath");
-    }
+export default function tildeImportsPlugin({ monorepoDirpath }) {
+  const expandTildeImport = createTildeImportExpander({
+    monorepoDirpath,
+  });
 
-    const expandTildeImport = createTildeImportExpander({
-      monorepoDirpath,
-    });
-
-    build.onResolve({ filter: /^~/ }, (args) => {
-      return {
-        path: expandTildeImport({
-          importSpecifier: args.path,
-          importerFilepath: args.importer,
-        }),
-        namespace: "glob-imports",
-      };
-    });
-  },
-};
-
-export default tildeImportsPlugin;
+  return {
+    name: "bun-plugin-tilde-imports",
+    setup(build) {
+      build.onResolve({ filter: /^~/ }, (args) => {
+        return {
+          path: expandTildeImport({
+            importSpecifier: args.path,
+            importerFilepath: args.importer,
+          }),
+        };
+      });
+    },
+  };
+}
--- test/fixtures/tilde-import/scripts/build.ts
+++ test/fixtures/tilde-import/scripts/build.ts
@@ -1,8 +1,11 @@
 import tildeImportPlugin from "../../../../src/index.js";
+import { getMonorepoDirpath } from "get-monorepo-root";
 import Bun from "bun";
 
 Bun.build({
   entrypoints: ["../src/index.ts"],
   outdir: "../dist",
-  plugins: [tildeImportPlugin],
+  plugins: [
+    tildeImportPlugin({ monorepoDirpath: getMonorepoDirpath(__dirname) }),
+  ],
 });
