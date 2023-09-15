6918caafc90ba2b324e1eb85dda1449191db80e9
Leon Si <leon@leondreamed.com>
2023-09-14 22:54:47 -0400

    fix

--- test/fixtures/tilde-import/scripts/build.js
+++ test/fixtures/tilde-import/scripts/build.js
@@ -2,12 +2,13 @@
 
 import tildeImportPlugin from '../../../../src/index.js';
 import { getMonorepoDirpath } from 'get-monorepo-root';
-import * as esbuild from '../../../../node_modules/esbuild';
+import * as esbuild from 'esbuild';
+import { join } from 'desm'
 
 await esbuild.build({
 	entryPoints: ['../src/index.ts'],
 	outdir: '../dist',
 	plugins: [
-		tildeImportPlugin({ monorepoDirpath: getMonorepoDirpath(__dirname) })
+		tildeImportPlugin({ monorepoDirpath: getMonorepoDirpath(import.meta.url) })
 	]
 });
